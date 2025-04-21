f1e3d4a60ae29b0d17f85e34430323290d678d9b
HalaKorhani <halakorhani2005@icloud.com>
2025-04-21 21:53:40 +0300

    Retrieve logged-in user data from sessionStorage and display welcome message

--- Auth/scripts/home.js
+++ Auth/scripts/home.js
@@ -3,4 +3,20 @@ document.addEventListener("DOMContentLoaded", function () {
     const quizButton = document.querySelector(".btn1");
     const logoutButton = document.querySelector(".logout-btn");
   });
-  
+  const user = sessionStorage.getItem("loggedInUser");
+console.log("user", user);
+if (user) {
+  const fullName = JSON.parse(user).fullName;
+  const welcomeMessage = `hello ${fullName} in HalaQuiz l hlwi we amira we yali ma fi nema bl deni klaaaaaa we yali bda tsir ahla dc nashallah`;
+  messageDiv.textContent = welcomeMessage;
+  messageDiv.style.fontSize = "20px";
+  messageDiv.style.fontWeight = "bold";
+  messageDiv.style.color = "#7880F6";
+  messageDiv.style.textAlign = "center";
+  messageDiv.style.marginBottom = "20px";
+  quizButton.style.display = "inline-block";
+  logoutButton.style.display = "inline-block";
+} else {
+  quizButton.style.display = "none";
+  logoutButton.style.display = "none";
+}
