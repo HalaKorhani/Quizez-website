795054a1f7afffcb251f120c7dfc3cde11f24b97
HalaKorhani <halakorhani2005@icloud.com>
2025-04-21 22:09:56 +0300

    Select form input elements for full name, username, email, phone, password, and confirm password.

--- Auth/scripts/register.js
+++ Auth/scripts/register.js
@@ -1,4 +1,21 @@
 document.addEventListener("DOMContentLoaded", function () {
     const registerForm = document.querySelector("form");
   });
-  
+  const fullNameInput = registerForm.querySelector(
+    'input[placeholder="Enter your name"]'
+  );
+  const usernameInput = registerForm.querySelector(
+    'input[placeholder="Enter your username"]'
+  );
+  const emailInput = registerForm.querySelector(
+    'input[placeholder="Enter your email"]'
+  );
+  const phoneInput = registerForm.querySelector(
+    'input[placeholder="Enter your phone number"]'
+  );
+  const passwordInput = registerForm.querySelector(
+    'input[placeholder="Enter your password"]'
+  );
+  const confirmPasswordInput = registerForm.querySelector(
+    'input[placeholder="Confirm your password"]'
+  );
