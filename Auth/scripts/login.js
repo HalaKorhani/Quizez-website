c693e4fca4a30fbdf69b8a453063c19b979354db
HalaKorhani <halakorhani2005@icloud.com>
2025-04-21 22:26:45 +0300

    Set up DOMContentLoaded event and login form field selectors

---
+++ Auth/scripts/login.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const loginForm = document.querySelector("form");
+    const emailInput = document.querySelector("input[type='email']");
+    const passwordInput = document.querySelector("input[type='password']");
+  
