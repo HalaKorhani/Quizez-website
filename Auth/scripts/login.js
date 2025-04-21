99a736f03f3b6be16410eeda9c0377aedd761c3c
HalaKorhani <halakorhani2005@icloud.com>
2025-04-21 22:27:01 +0300

    Add admin login check and redirect to dashboard

--- Auth/scripts/login.js
+++ Auth/scripts/login.js
@@ -2,4 +2,14 @@ document.addEventListener("DOMContentLoaded", function () {
     const loginForm = document.querySelector("form");
     const emailInput = document.querySelector("input[type='email']");
     const passwordInput = document.querySelector("input[type='password']");
-  
+    loginForm.addEventListener("submit", function (e) {
+        e.preventDefault();
+    
+        const email = emailInput.value.trim();
+        const password = passwordInput.value;
+    
+        if (email === "admin@quiz.com" && password === "admin123") {
+          window.location.href = "../dashoard/dashboard.html";
+          return;
+        }
+    
